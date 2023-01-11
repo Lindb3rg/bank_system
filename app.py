6807c5b16b876c78e1e104cef574458098ffadaa
Adam Lindberg <ad.lindberg@gmail.com>
2023-01-11 20:05:31 +0100

    kundsida_medsök

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 from model import Customer,Account,Transaction
+from sqlalchemy import func
 
 from model import db, seedData
 
@@ -19,18 +20,60 @@ migrate = Migrate(app,db)
 def startpage():
     total_customers = len(Customer.query.all())
     total_accounts = len(Account.query.all())
-    total_transactions = len(Transaction.query.all())
-
-
-
-
-    return render_template("index.html", total_customers=total_customers,total_accounts=total_accounts,total_transactions=total_transactions)
+    total_balance = Account.query.with_entities(func.sum(Account.Balance).label('total')).first().total
+    return render_template("index.html", total_customers=total_customers,total_accounts=total_accounts,total_balance=total_balance)
 
 
 @app.route("/customers")
-def customers():
-    customers = Customer.query.all()
-    return render_template("customers.html", customers=customers)
+def customerspage():
+    sortColumn = request.args.get('sortColumn', 'namn')
+    sortOrder = request.args.get('sortOrder', 'asc')
+    page = int(request.args.get('page', 1))
+    searchWord = request.args.get('q','')
+
+    listOfCustomers = Customer.query
+
+    #sökning i databas
+    listOfCustomers = listOfCustomers.filter(
+        Customer.GivenName.like('%' + searchWord + '%') |
+        Customer.Country.like('%' + searchWord + '%') |    
+        Customer.City.like('%' + searchWord + '%' ))
+        
+
+    if sortColumn == "namn":
+        if sortOrder == "asc":
+            listOfCustomers = listOfCustomers.order_by(Customer.GivenName.asc())
+        else:
+            listOfCustomers = listOfCustomers.order_by(Customer.GivenName.desc())
+    elif sortColumn == "city":
+        if sortOrder == "asc":
+            listOfCustomers = listOfCustomers.order_by(Customer.City.asc())
+        else:
+            listOfCustomers = listOfCustomers.order_by(Customer.City.desc())
+    elif sortColumn == "country":
+        if sortOrder == "asc":
+            listOfCustomers = listOfCustomers.order_by(Customer.Country.asc())
+        else:
+            listOfCustomers = listOfCustomers.order_by(Customer.Country.desc())
+
+    paginationObject = listOfCustomers.paginate(page=page,per_page=20,error_out=False )
+    return render_template("customers.html", 
+                    listOfCustomers=paginationObject.items, 
+                    activePage="customersPage",
+                    page=page,
+                    sortColumn=sortColumn,
+                    sortOrder=sortOrder,
+                    has_next = paginationObject.has_next,
+                    has_prev = paginationObject.has_prev,
+                    pages=paginationObject.pages,
+                    q = searchWord)
+
+
+
+# @app.route("/customers")
+# def customers():
+#     customers = Customer.query.all()
+#     return render_template("customers.html", customers=customers)
 
 
 @app.route("/customer/<id>")
@@ -42,7 +85,8 @@ def customer_page(id):
 if __name__  == "__main__":
     with app.app_context():
         upgrade()
-    
+
+
         seedData(db)
         app.run()
 
