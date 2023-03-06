ce6689ebee85d82bc72f807c1b750476fa7afad4
Adam Lindberg <ad.lindberg@gmail.com>
2023-03-06 01:24:58 +0100

    städ

--- forms.py
+++ forms.py
@@ -83,14 +83,6 @@ def not_current_user(form,field):
 
 
 
-class Issue_report_form(FlaskForm):
-    name = StringField('name', validators=[validators.DataRequired()])
-    email = EmailField('email', validators=[validators.DataRequired(), validators.Email()])
-    userid = IntegerField('userid', validators=[validators.DataRequired()])
-    textfield = TextAreaField('textfield', validators=[validators.DataRequired()])
-    p1 = BooleanField("p1")
-    p2 = BooleanField("p2")
-    p3 = BooleanField("p3")
 
 
 
