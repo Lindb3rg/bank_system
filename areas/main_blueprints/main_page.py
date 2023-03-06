5e6cee7b6bf5a986ffec463665c08f5491c9296d
Adam Lindberg <ad.lindberg@gmail.com>
2023-03-06 01:10:37 +0100

    main_page_klar

--- areas/main_blueprints/main_page.py
+++ areas/main_blueprints/main_page.py
@@ -17,29 +17,69 @@ main_page = Blueprint('main_page', __name__)
 @roles_accepted("Admin","Cashier")
 def startpage():
 
-    # US_customers = Customer.query.filter_by(Country="USA").all()
-    # US_count = len(US_customers)
+    US_customers = Customer.query.filter_by(Country="USA").all()
+    US_count = len(US_customers)
+    US_total_balance = 0
+    US_total_accounts = 0
     US_results = db.session.query(Customer, Account).join(Account).filter(Customer.Country=="USA").all()
-    US_count = len(US_results.Accounts)
+
+    for customer,account in US_results:
+        US_total_balance += account.Balance
+        US_total_accounts += 1
+
 
 
     SE_customers = Customer.query.filter_by(Country="Sweden").all()
     SE_count = len(SE_customers)
+    SE_total_balance = 0
+    SE_total_accounts = 0
+    SE_results = db.session.query(Customer, Account).join(Account).filter(Customer.Country=="Sweden").all()
+
+    for customer,account in SE_results:
+        SE_total_balance += account.Balance
+        SE_total_accounts += 1
+
+
+
 
     FI_customers = Customer.query.filter_by(Country="Finland").all()
     FI_count = len(FI_customers)
+    FI_total_balance = 0
+    FI_total_accounts = 0
+    FI_results = db.session.query(Customer, Account).join(Account).filter(Customer.Country=="Finland").all()
+
+    for customer,account in FI_results:
+        FI_total_balance += account.Balance
+        FI_total_accounts += 1
 
     NO_customers = Customer.query.filter_by(Country="Norway").all()
     NO_count = len(NO_customers)
+
+    NO_total_balance = 0
+    NO_total_accounts = 0
+    NO_results = db.session.query(Customer, Account).join(Account).filter(Customer.Country=="Norway").all()
+
+    for customer,account in NO_results:
+        NO_total_balance += account.Balance
+        NO_total_accounts += 1
    
     
 
 
     
     return render_template("/main_templates/start_page.html", US_count=US_count,
+                           US_total_balance = US_total_balance,
+                           US_total_accounts = US_total_accounts,
                            SE_count=SE_count,
+                           SE_total_accounts = SE_total_accounts,
+                           SE_total_balance = SE_total_balance,
+
                            FI_count=FI_count,
-                           NO_count=NO_count)
+                           FI_total_accounts = FI_total_accounts,
+                           FI_total_balance = FI_total_balance,
+                           NO_count=NO_count,
+                           NO_total_accounts = NO_total_accounts,
+                           NO_total_balance = NO_total_balance)
 
 
 
